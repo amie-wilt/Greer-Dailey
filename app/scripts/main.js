d5630857e25649de6d37b006b717d04f6a4413c4
Dan Wilt <dwilt4rville@gmail.com>
2015-03-21 13:49:45 -0700

    lots of css and js work with Amie

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
 'use strict';
 
-(function ($) {
-    $(function () {
+(function($) {
+
+    $('.mobile-menu-trigger').on('click', function() {
+        $('.header-primary-nav-container').toggleClass('mobile-menu-active');
     });
+
+    $('.expertise-section-open-trigger').on('click', function(e) {
+        e.preventDefault();
+        $($(this).attr('href')).slideToggle();
+    });
+
+    $('.expertise-section-top-close-button, .expertise-section-bottom-close-button').on('click', function() {
+        $(this).closest('.expertise-section').slideUp();
+    });
+
 })(jQuery);
