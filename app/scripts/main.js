a6a94a867631a1cd40d6f4d6b1a322999cf44238
Amie Wilt <amie.wilt@gmail.com>
2015-03-22 12:00:47 -0400

    worked on mobile responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,11 +8,16 @@
 
     $('.expertise-section-open-trigger').on('click', function(e) {
         e.preventDefault();
-        $($(this).attr('href')).slideToggle();
+        $($(this).attr('href'))
+            .find('.expertise-header-summary-wrapper')
+            .slideToggle();
     });
 
     $('.expertise-section-top-close-button, .expertise-section-bottom-close-button').on('click', function() {
-        $(this).closest('.expertise-section').slideUp();
+        $(this)
+            .closest('.expertise-section')
+            .find('.expertise-header-summary-wrapper')
+            .slideUp();
     });
 
 })(jQuery);
