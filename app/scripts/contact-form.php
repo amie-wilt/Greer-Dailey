c09357e4e309ce76374db65e7283f5b638845443
Amie Wilt <amie.wilt@gmail.com>
2015-04-19 16:58:22 -0400

    removed php from contact hbs

---
+++ app/scripts/contact-form.php
@@ -0,0 +1,17 @@
+<?php
+
+    $subject = "Contact Form Submission";
+
+    $usersubject = 'Thank you! from the-girlfriends.com';
+    $userbody.= '<div><h1 style="text-align: center;color:#F3600C;margin-bottom: 20px;">Thank you for your submission!</h1></div>';
+
+    $body = '<div style="padding:bottom:5px;border-bottom:1px solid #d9dcdd;margin-bottom:5px;"><span><strong style="color:#F3600C;">First Name:</strong></span> <span>'.$_POST['contact-form-first-name'].'</span></div>';
+    $body.= '<div style="padding:bottom:5px;border-bottom:1px solid #d9dcdd;margin-bottom:5px;"><span><strong style="color:#F3600C;">Last Name:</strong></span> <span>'.$_POST['contact0-form-last-name'].'</span></div>';
+    $body.= '<div style="padding:bottom:5px;border-bottom:1px solid #d9dcdd;margin-bottom:5px;"><span><strong style="color:#F3600C;">Email:</strong></span> <span>'.$_POST['contact-form-email'].'</span></div>';
+    $body.= '<div><span><strong style="color:#F3600C;">Comments/Questions:</strong></span> <span>'.$_POST['questions-comments'].'</span></div>';
+
+    $headers = 'Content-Type: text/html' . "\r\n";
+    $headers.= $_POST['contact'] . "\r\n";
+    mail( $_POST['email'], $usersubject, $body, $headers);
+    mail( $_POST['contact'], $subject, $body, $headers) or die('test');
+?>
