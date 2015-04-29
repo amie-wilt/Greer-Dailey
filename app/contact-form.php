60691472ef1f4727cfbce896a19ea63539d37f83
Amie Wilt <amie.wilt@gmail.com>
2015-04-29 12:35:21 -0400

    added style to form email

--- app/contact-form.php
+++ app/contact-form.php
@@ -19,7 +19,7 @@ $fullName = $firstName . ' ' . $lastName;
 
 //Email Body
 $emailBody = '';
-$emailBody = $emailBody . 'First Name: ' . $firstName . '<br>';
+$emailBody = $emailBody . '<div style="padding:bottom:5px;border-bottom:1px solid #d9dcdd;margin-bottom:5px;"><span><strong style="color:#F3600C;">First Name: </strong></span> <span>' . $firstName . '</span></div><br>';
 $emailBody = $emailBody . 'Last Name: ' . $lastName . '<br>';
 $emailBody = $emailBody . 'Email: ' . $email . '<br>';
 $emailBody = $emailBody . 'Comments/Questions: ' . $message;
